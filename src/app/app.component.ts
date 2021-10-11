571fd33a823da9d8de6dcb01f454215c81a7d144
JocoolQ <jscaperaq@correo.udistrital.edu.co>
2021-10-11 14:34:52 -0500

    fix: se cambia libreria y se ajusta comportamiento de reCAPTCHA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { AstTransformer } from '@angular/compiler/src/output/output_ast';
+import { Component, ElementRef, OnInit, ViewChild, ViewChildren } from '@angular/core';
 import { environment } from 'src/environments/environment';
+
+
+
 
 
 @Component({
@@ -10,12 +14,16 @@ import { environment } from 'src/environments/environment';
 export class AppComponent implements OnInit{
   loadRouting = false;
   environment = environment;
+  @ViewChildren('ng-uui-oas') allChildren;
 
   title = 'verificacion-certificados-cliente';
 
 
   ngOnInit(): void {
     const oas = document.querySelector('ng-uui-oas');
+    console.log(oas.setAttribute("isLogin","true"))
+    console.log(oas.childNodes)
+    
     this.loadRouting = true;
 
   }
--- src/app/pages/verificacion-informacion/verificacion-informacion.component.ts
+++ src/app/pages/verificacion-informacion/verificacion-informacion.component.ts
@@ -18,28 +18,28 @@ export class VerificacionInformacionComponent implements OnInit {
   formModel: FormModel = {};
   documento: Documento;
   hash: string = '';
-  key: string='';//variable que contiene el valor del sitekey del captcha
+  key: string = '';//variable que contiene el valor del sitekey del captcha
   source: LocalDataSource = new LocalDataSource();
   settings: any;
 
   constructor(
     private request: RequestManager
 
-  ) { 
+  ) {
 
   }
 
-  cargarSiteKey(){
+  cargarSiteKey() {
     this.request.get(environment.ACADEMICA_JBPM_SERVICE, 'sitekey')
-    .subscribe((sitekey: any) => {
-      if (sitekey) {
-        this.key = sitekey['sitekey']['key'];
-        console.log(this.key);
-        //captchaProtectedForm.form.valid;
-      }
-    }, (error) => {
-      console.log(error);
-    })
+      .subscribe((sitekey: any) => {
+        if (sitekey) {
+          this.key = sitekey['sitekey']['key'];
+          console.log(this.key);
+          //captchaProtectedForm.form.valid;
+        }
+      }, (error) => {
+        console.log(error);
+      })
 
   }
 
@@ -98,32 +98,37 @@ export class VerificacionInformacionComponent implements OnInit {
   }
 
   public verificarDocumento() {
-    this.request.get(environment.ACADEMICA_JBPM_SERVICE, 'validar_hash/'+this.hash)
-    .subscribe((documentos: any) => {
-      if (documentos) {
-        this.documento = documentos['documentos'];
-        var info_documentos=[];
-        info_documentos.push(this.documento);
-        let datosDocumentos = info_documentos.map((doc) =>
-          new Object({
-            Codigo: `${doc.codigo}`,
-            Nombre: `${doc.nombre}`,
-            Identificacion: `${doc.identificacion}`,
-            Nota: `${doc.nota}`,
-            FechaGrado: `${doc.fechaGrado}`,
-            Fecha: `${doc.fecha}`,
-            TipoDocumento:`${doc.tipoDocumento}`,
-            NumeroActa: `${doc.numeroActa}`,
-            Proyecto: `${doc.proyecto}`,
-          
-          }))
-        this.source.load(datosDocumentos);
-          console.log(this.source);
-      }
-    }, (error) => {
-      console.log(error);
-      //Swal.close();
-    })
+    this.formModel.captcha = ''  //(opcional) reinicia el captcha en cada petición
+    this.request.get(environment.ACADEMICA_JBPM_SERVICE, 'validar_hash/' + this.hash)
+      .subscribe((documentos: any) => {
+        if (documentos) {
+          this.documento = documentos['documentos'];
+          if (!(Object.keys(this.documento).length === 0 && this.documento.constructor === Object)) {
+            var info_documentos = [];
+            info_documentos.push(this.documento);
+            let datosDocumentos = info_documentos.map((doc) =>
+              new Object({
+                Codigo: `${doc.codigo}`,
+                Nombre: `${doc.nombre}`,
+                Identificacion: `${doc.identificacion}`,
+                Nota: `${doc.nota}`,
+                FechaGrado: `${doc.fechaGrado}`,
+                Fecha: `${doc.fecha}`,
+                TipoDocumento: `${doc.tipoDocumento}`,
+                NumeroActa: `${doc.numeroActa}`,
+                Proyecto: `${doc.proyecto}`,
+              }))
+              
+            this.source.load(datosDocumentos);
+            console.log(this.source);
+          }else{
+            this.source.load([]);
+          }
+        }
+      }, (error) => {
+        console.log(error);
+        //Swal.close();
+      })
   }
 
   ngOnInit(): void {
